3ab2e8633cfe79497737339778379abf7b0483e1
5592costa <rodrigo5592@yahoo.com.br>
2024-12-03 14:13:32 -0300

    Atualizacao

---
+++ Aulas/Aula1_conceitos_iniciais/minha_classe.py
@@ -0,0 +1,8 @@
+class MinhaClasse:
+
+    def metodo_1(self):
+        print("minha acao 1")
+        
+# objeto        #classe -> instanciamos um objeto
+minha_classe = MinhaClasse()
+minha_classe.metodo_1()
